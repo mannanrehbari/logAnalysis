5ee62e4740defee8e3e3f343062e94b5b0524bff
Abdul Mannan <abdul.mannan@mavs.uta.edu>
2019-01-17 00:03:20 -0600

    Major query fixes, change in structure with updates to README

--- README.md
+++ README.md
@@ -1,25 +1,37 @@
 # Log Analysis
 
-Log Analysis is a small project that reflects basic understanding of Python DB API and PGSQL statements. It is built on Vagrant VM with Python and PostgreSQL.
+Log Analysis script uses psycopg2 to query a mock PostgreSQL database for a fictional news website.
+There are three tables in the **news** database: __articles__, __authors__, and __logs__. The structure
+of each table can be accessed in psql using **\d example_table** command. __Articles__ has the following columns: author, title, slug, lead, body, time, and id.  __Authors__ has following columns: name, bio, and id. __log__ has the following columns: path, ip, method, status, time, and id. The logAnalysis.py script generates results for top articles, top authors, and date with highest '404 NOT FOUND' errors. In the same order, and saves it in output. The SQL queries can be seen inside __logAnalysis.py__ file.
+
 
 ## Dependencies
-This project assumes that Python 2.7.12 and PGSQL (PostgreSQL 9.5.14) are installed on vagrant VM machine with Ubuntu 16.04.5 LTS.
+This project assumes that Python 2.7.12 and PostgreSQL 9.5.14 . This script was run on Vagrant v2.2.2 software (which runs on VirtualBox VM) installed on Windows 10 64bit with Ubuntu 16.04.5 LTS. Vagrant configuration file can be downloaded from this repository, and the following link can be used for downloading Vagrant:
+[Vagrant](https://www.vagrantup.com/downloads.html)
 
 ## Usage
-To load the database into psql type the following command:
-`` psql -d news -f newsdata.sql ``
-This command will load data from **newsdata.sql** into **news** file, the latter can be run using psql.
+The mock news data can be downloaded from [here](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip). The Vagrantfile generates the __news__ database, but can also be generated manually by using the command ``psql -d news -f newsdata.sql ``.
 
 This program is dependent on two views that need to be created. To create views, run:
 `psql news`  on vagrant in the logAnalysis directory. Once the psql command-line is ready, create the following views.
 
 The first view:
 
-`CREATE VIEW error_status AS SELECT time::DATE, COUNT(*) FROM log WHERE status = '404 NOT FOUND' GROUP BY time::DATE;`
+```sql
+CREATE VIEW error_status AS
+SELECT time::DATE, COUNT(*)
+FROM log WHERE status = '404 NOT FOUND'
+GROUP BY time::DATE;
+```
 
 The second view:
 
-`CREATE VIEW all_status AS SELECT time::DATE, COUNT(*) FROM log GROUP BY time::DATE;`
+```sql
+CREATE VIEW all_status AS
+SELECT time::DATE, COUNT(*)
+FROM log
+GROUP BY time::DATE;
+```
 
 After creating views, exit out of psql command-line by pressing CTRL + D. Once back in the vagrant terminal, type the following to run the program: `python logAnalysis.py`
 
--- logAnalysis.py
+++ logAnalysis.py
@@ -1,80 +1,87 @@
 #!/usr/bin/env python
 import psycopg2
 
-
-# function for log analysis will be called in main
-def logAn():
-    # connection object: if the db 'news exists' program will proceed
+def db_connection(dbname):
+    """ Connects to db, returns db, cursor """
     try:
-        conn = psycopg2.connect("dbname=news")
+        conn = psycopg2.connect(dbname)
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
-        quit()
-    # cursor object: can be used to perform queries
+
     curs = conn.cursor()
 
-    # textfile for output
-    text_file = open("Output.txt", "w")
+    return conn, curs
 
-    # first query: 3 most popular articles
+def execute_query(query, curs):
+    """ Executes the query provided in the parameter """
+    curs.execute(query)
+    results = curs.fetchall()
+    return results
+
+def top_articles(curs, text_file):
+    """ calls execute query for finding top articles """
     query1 = """SELECT A.title, L.views FROM articles A
                 INNER JOIN (SELECT path,
                 COUNT(*) AS views FROM log
                 GROUP BY path) L ON L.path ='/article/' || A.slug
                 ORDER BY views DESC
                 LIMIT 3;"""
+    results = execute_query(query1, curs)
 
-    # execute query1  and retrieve all results
-    curs.execute(query1)
-    record1 = curs.fetchall()
+    text_file.write("\n********** Top Articles **********\n");
+    for row in results:
+        text_file.write("{} -- {} views\n".format(row[0],row[1]))
 
-    text_file.write("--------   Query1   ---------\n")
-    for title, views in record1:
-        text_file.write("{} -- {} views\n".format(title,views))
 
-    # second query: author views descending
+
+def top_authors(curs, text_file):
+    """ Finds the authors with most views on articles"""
     query2 = """
-        SELECT au.name AS authName, count(*) AS views
-        FROM articles a
-        INNER JOIN log l
-        ON l.path = '/article/' || a.slug
-        INNER JOIN authors AS au
-        ON au.id = a.author
-        GROUP BY authName
-        ORDER BY views DESC;
-        """
-
-
-    curs.execute(query2)
-    record2 = curs.fetchall()
-
-    text_file.write("\n--------   Query2  ---------\n")
-    for row in record2:
+            SELECT au.name AS authName, count(*) AS views
+            FROM articles a
+            INNER JOIN log l
+            ON l.path = '/article/' || a.slug
+            INNER JOIN authors AS au
+            ON au.id = a.author
+            GROUP BY authName
+            ORDER BY views DESC;
+            """
+    text_file.write("\n********** Top Authors **********\n");
+    results = execute_query(query2, curs)
+    for row in results:
         text_file.write("{} -- {} views\n".format(row[0], row[1]))
 
-    # third query: percent errors
-    # the assumption is that views are already created
-
+def over_1_pc(curs, text_file):
+    """ Finds days where the error logs are over 1% of total logs """
     query3 = """select
-                round((100.00 * error_status.count/all_status.count)::numeric,2)
+                round((100.00 * error_status.count/all_status.count)::numeric,1)
                 as fracT, error_status.time
                 from error_status join all_status
                 on error_status.time = all_status.time
                 where round(100*error_status.count/all_status.count) > 1;"""
-    curs.execute(query3)
-    record3 = curs.fetchall()
-    text_file.write("\n--------    Query3   ---------\n")
-    for row in record3:
+    text_file.write("\n********** Over 1% errors on **********\n");
+    results = execute_query(query3, curs)
+    for row in results:
         text_file.write("{} -- {} % errors\n".format(row[1], row[0]))
 
-    print("Program results saved in Output.txt")
+    text_file.write("\n");
 
-    # closing the text file, open cursor, and connection
-    text_file.close()
-    curs.close()
-    conn.close()
 
 
 if __name__ == "__main__":
-    # function call: does everything when the program is run
-    logAn()
+    """ main function calls query functions  """
+    dbname = "dbname=news"
+    text_file = open("Output.txt", "w+")
+    conn, curs = db_connection(dbname)
+
+    print("Connection established! Please wait till processing is complete...")
+
+    top_articles(curs, text_file)
+    top_authors(curs, text_file)
+    over_1_pc(curs, text_file)
+
+    print("Done! The results were saved in Output.txt")
+
+    text_file.close()
+    curs.close()
+    conn.close()
